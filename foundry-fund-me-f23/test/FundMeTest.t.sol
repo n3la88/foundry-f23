09691721d1961696f2cc4ac920550fb5a80c8e18
Marianela Montoya Abarca <mari.montab@gmail.com>
2024-11-30 22:02:37 -0600

    Refactoring tests

--- foundry-fund-me-f23/test/FundMeTest.t.sol
+++ foundry-fund-me-f23/test/FundMeTest.t.sol
@@ -3,13 +3,16 @@ pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
 
     //setUp runs first
     function setUp() external {
-        fundMe = new FundMe();
+        //fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
     }
 
     function testMinDollarsIsFive() public view {
@@ -20,6 +23,23 @@ contract FundMeTest is Test {
         console.log(fundMe.i_owner());
         console.log(address(this));
         console.log(msg.sender);
-        assertEq(fundMe.i_owner(), address(this));
+        assertEq(fundMe.i_owner(), msg.sender);
+    }
+
+    function testPriceFeedVersionIsAccurate() public view {
+        uint256 version = fundMe.getVersion();
+        assertEq(version, 4);
     }
 }
+
+/* To work with addresses outside our system
+
+1. Unit 
+    - Testing specific part of the code
+2. Integration
+    - Testing how our code works with other parts of our code
+3. Forked
+    - Testing our code on a simulated real environment
+4. Staging
+    - Testing our code in a real environment that is not real 
+*/
